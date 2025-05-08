8a99b7d2fd65f6e54128a8a25ae4e9c9fcf23435
Mario Maxim <maximmarioandrei@gmail.com>
2025-05-08 20:43:55 +0300

    feat: restructure project to match required layout

---
+++ src/test/java/com/siemens/internship/ItemServiceTest.java
@@ -0,0 +1,12 @@
+package com.siemens.internship;
+
+import com.siemens.internship.model.Item;
+import com.siemens.internship.repository.ItemRepository;
+import com.siemens.internship.service.ItemService;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemServiceTest {
+
+}
